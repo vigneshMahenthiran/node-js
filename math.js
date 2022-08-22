55afb2e135c60307b927d02337327f4a01df0490
vigneshMahenthiran <106167038+vigneshMahenthiran@users.noreply.github.com>
2022-08-22 20:14:26 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,9 @@
+const a = 20;
+const b = 10;
+console.log(`the value of A = ${a}`);
+console.log(`the value of B = ${b}`);
+console.log("lets do the mathematical operations....");
+console.log("addition of A and B is ",(a+b));
+console.log("subraction of A and B is ",(a-b));
+console.log("multiplication of A and B is ",(a*b));
+console.log("division of A and B is ",(a/b));
