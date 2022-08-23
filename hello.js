704233413c1c7a4adf6fa8f109a1c25d0dd344c7
vigneshMahenthiran <106167038+vigneshMahenthiran@users.noreply.github.com>
2022-08-23 14:10:47 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+// console.log('hello world');
+const a = 'hello';
+const b = 'world';
+const c = a + b;
+console.log(c);
+
+
+console.log('hi.... : ', c);
+console.log(`hi.... : ${c}`);
+console.log()
+
+// let, const
