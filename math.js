89a0ddfbf0340b0ee18353c2d98c8e2230434f8e
vigneshMahenthiran <106167038+vigneshMahenthiran@users.noreply.github.com>
2022-08-23 13:00:56 +0530

    math.js

--- math.js
+++ math.js
@@ -1,5 +1,7 @@
 const a = 20;
 const b = 10;
+let s1 = "indium";
+let s2= "software";
 console.log(`the value of A = ${a}`);
 console.log(`the value of B = ${b}`);
 console.log("lets do the mathematical operations....");
@@ -7,3 +9,6 @@ console.log("addition of A and B is ",(a+b));
 console.log("subraction of A and B is ",(a-b));
 console.log("multiplication of A and B is ",(a*b));
 console.log("division of A and B is ",(a/b));
+console.log(`the value of String1 = ${s1}`);
+console.log(`the value of String2 = ${s2}`);
+console.log("concatenation of string1 and string2 is ",(s1+s2));
