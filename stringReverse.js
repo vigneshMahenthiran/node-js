b5a0a9a7baa42e3f792857ddb64abcfc55a075bb
vigneshMahenthiran <vignesh.mahenthiran@indiumsoft.com>
2022-10-25 20:01:28 +0530

    reversing each word in a array

--- stringReverse.js
+++ stringReverse.js
@@ -1,5 +1,5 @@
 let str = "My name is Vinz";
-str = str.split(' ');
+str2 = str.split(' ');
 let str3 = "";
 for(let i = 0; i < str2.length; i++){
     str2[i] = str2[i].split('').reverse().join('');
